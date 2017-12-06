ef6570e6b5dd26d572d46f5cfc7fd469db200b04
jpdempsey317 <jpdempsey317@gmail.com>
2017-12-05 22:20:35 -0500

    Initial Commit

---
+++ CDBrowser001/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace CDBrowser001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pbAdobe = new System.Windows.Forms.PictureBox();
+            this.pbGoogle = new System.Windows.Forms.PictureBox();
+            this.lblAdobe = new System.Windows.Forms.Label();
+            this.pbStratTrader = new System.Windows.Forms.PictureBox();
+            this.lblGoogle = new System.Windows.Forms.Label();
+            this.lblStratTrader = new System.Windows.Forms.Label();
+            this.pbIE = new System.Windows.Forms.PictureBox();
+            this.lblIE = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdobe)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGoogle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStratTrader)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIE)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbAdobe
+            // 
+            this.pbAdobe.BackColor = System.Drawing.Color.Transparent;
+            this.pbAdobe.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbAdobe.Location = new System.Drawing.Point(12, 226);
+            this.pbAdobe.Name = "pbAdobe";
+            this.pbAdobe.Size = new System.Drawing.Size(140, 32);
+            this.pbAdobe.TabIndex = 0;
+            this.pbAdobe.TabStop = false;
+            this.pbAdobe.Tag = "";
+            this.pbAdobe.Click += new System.EventHandler(this.pbAdobe_Click);
+            // 
+            // pbGoogle
+            // 
+            this.pbGoogle.BackColor = System.Drawing.Color.Transparent;
+            this.pbGoogle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pbGoogle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbGoogle.Location = new System.Drawing.Point(12, 185);
+            this.pbGoogle.Name = "pbGoogle";
+            this.pbGoogle.Size = new System.Drawing.Size(147, 35);
+            this.pbGoogle.TabIndex = 1;
+            this.pbGoogle.TabStop = false;
+            this.pbGoogle.Click += new System.EventHandler(this.pbGoogle_Click);
+            // 
+            // lblAdobe
+            // 
+            this.lblAdobe.AutoSize = true;
+            this.lblAdobe.BackColor = System.Drawing.Color.Transparent;
+            this.lblAdobe.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblAdobe.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAdobe.Location = new System.Drawing.Point(18, 237);
+            this.lblAdobe.Name = "lblAdobe";
+            this.lblAdobe.Size = new System.Drawing.Size(117, 13);
+            this.lblAdobe.TabIndex = 2;
+            this.lblAdobe.Text = "Click Here for Adobe";
+            this.lblAdobe.Click += new System.EventHandler(this.lblAdobe_Click);
+            // 
+            // pbStratTrader
+            // 
+            this.pbStratTrader.BackColor = System.Drawing.Color.Transparent;
+            this.pbStratTrader.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbStratTrader.Location = new System.Drawing.Point(12, 84);
+            this.pbStratTrader.Name = "pbStratTrader";
+            this.pbStratTrader.Size = new System.Drawing.Size(147, 33);
+            this.pbStratTrader.TabIndex = 3;
+            this.pbStratTrader.TabStop = false;
+            this.pbStratTrader.Click += new System.EventHandler(this.pbStratTrader_Click);
+            // 
+            // lblGoogle
+            // 
+            this.lblGoogle.AutoSize = true;
+            this.lblGoogle.BackColor = System.Drawing.Color.Transparent;
+            this.lblGoogle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblGoogle.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGoogle.Location = new System.Drawing.Point(12, 194);
+            this.lblGoogle.Name = "lblGoogle";
+            this.lblGoogle.Size = new System.Drawing.Size(169, 13);
+            this.lblGoogle.TabIndex = 4;
+            this.lblGoogle.Text = "Click Here for Google Chrome";
+            this.lblGoogle.Click += new System.EventHandler(this.lblGoogle_Click);
+            // 
+            // lblStratTrader
+            // 
+            this.lblStratTrader.AutoSize = true;
+            this.lblStratTrader.BackColor = System.Drawing.Color.Transparent;
+            this.lblStratTrader.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblStratTrader.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStratTrader.ForeColor = System.Drawing.Color.Black;
+            this.lblStratTrader.Location = new System.Drawing.Point(12, 91);
+            this.lblStratTrader.Name = "lblStratTrader";
+            this.lblStratTrader.Size = new System.Drawing.Size(177, 13);
+            this.lblStratTrader.TabIndex = 5;
+            this.lblStratTrader.Text = "Click Here to Install StratTrader";
+            this.lblStratTrader.Click += new System.EventHandler(this.lblStratTrader_Click);
+            // 
+            // pbIE
+            // 
+            this.pbIE.BackColor = System.Drawing.Color.Transparent;
+            this.pbIE.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pbIE.Location = new System.Drawing.Point(12, 138);
+            this.pbIE.Name = "pbIE";
+            this.pbIE.Size = new System.Drawing.Size(147, 36);
+            this.pbIE.TabIndex = 6;
+            this.pbIE.TabStop = false;
+            this.pbIE.Click += new System.EventHandler(this.pbIE_Click);
+            // 
+            // lblIE
+            // 
+            this.lblIE.AutoSize = true;
+            this.lblIE.BackColor = System.Drawing.Color.Transparent;
+            this.lblIE.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblIE.Font = new System.Drawing.Font("Times New Roman", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIE.Location = new System.Drawing.Point(18, 144);
+            this.lblIE.Name = "lblIE";
+            this.lblIE.Size = new System.Drawing.Size(134, 13);
+            this.lblIE.TabIndex = 7;
+            this.lblIE.Text = "Install Internet Explorer";
+            this.lblIE.Click += new System.EventHandler(this.lblIE_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(432, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 22);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Close";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::CDBrowser001.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(500, 280);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblIE);
+            this.Controls.Add(this.pbIE);
+            this.Controls.Add(this.lblStratTrader);
+            this.Controls.Add(this.lblGoogle);
+            this.Controls.Add(this.pbStratTrader);
+            this.Controls.Add(this.lblAdobe);
+            this.Controls.Add(this.pbGoogle);
+            this.Controls.Add(this.pbAdobe);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DAC CD Browser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbAdobe)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGoogle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbStratTrader)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbIE)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbAdobe;
+        private System.Windows.Forms.PictureBox pbGoogle;
+        private System.Windows.Forms.Label lblAdobe;
+        private System.Windows.Forms.PictureBox pbStratTrader;
+        private System.Windows.Forms.Label lblGoogle;
+        private System.Windows.Forms.Label lblStratTrader;
+        private System.Windows.Forms.PictureBox pbIE;
+        private System.Windows.Forms.Label lblIE;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ CDBrowser001/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace CDBrowser001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+        }
+
+        private void pbAdobe_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://get.adobe.com/reader/");
+          
+        }
+
+       
+
+        private void pbGoogle_Click(object sender, EventArgs e)
+        {
+
+            Process.Start("https://www.google.com/chrome/browser/desktop/index.html");
+        }
+
+        private void pbStratTrader_Click(object sender, EventArgs e)
+        {
+            Process.Start(".\\Setup\\setup.exe");
+        }
+
+        private void pbIE_Click(object sender, EventArgs e)
+        {
+            Process.Start(".\\Installs\\EIE11_EN-US_WOL_WIN764.EXE");
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void lblStratTrader_Click(object sender, EventArgs e)
+        {
+            //Process.Start("Setup\\setup.exe");
+            Process.Start("C:\\Users\\Patrick\\Documents\\Visual Studio 2013\\Projects\\CDBrowser001\\CDBrowser001\\Setup\\setup.exe");
+        }
+
+        private void lblIE_Click(object sender, EventArgs e)
+        {
+            //Process.Start("Installs\\EIE11_EN-US_WOL_WIN764.EXE");
+            Process.Start("C:\\Users\\Patrick\\Documents\\Visual Studio 2013\\Projects\\CDBrowser001\\CDBrowser001\\Installs\\EIE11_EN-US_WOL_WIN764.EXE");
+        }
+
+        private void lblGoogle_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://www.google.com/chrome/browser/desktop/index.html");
+        }
+
+        private void lblAdobe_Click(object sender, EventArgs e)
+        {
+            Process.Start("https://get.adobe.com/reader/");
+        }
+
+        
+    }
+}
